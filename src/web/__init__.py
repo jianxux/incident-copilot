c7933ef9a6fd97068c8eb02981b0fe874c9cabc9
jayceexu <xujaycee@gmail.com>
2026-02-01 18:08:28 -0800

    fix: sort imports with isort

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,6 +1,7 @@
 """Web dashboard module for Incident Copilot."""
 
-from .routes import landing_router, router as web_router
+from .routes import landing_router
+from .routes import router as web_router
 from .store import incident_store
 
 __all__ = ["web_router", "landing_router", "incident_store"]
