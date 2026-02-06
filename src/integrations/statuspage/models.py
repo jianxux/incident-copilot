a2064b11a1e9546cdc5a865a7129f1412397b1de
jayceexu <xujaycee@gmail.com>
2026-02-06 01:52:18 -0800

    fix: resolve lint issues

--- src/integrations/statuspage/models.py
+++ src/integrations/statuspage/models.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 from enum import Enum
 
-from pydantic import BaseModel, Field, HttpUrl
+from pydantic import BaseModel, Field
 
 
 class StatusPageProvider(str, Enum):
